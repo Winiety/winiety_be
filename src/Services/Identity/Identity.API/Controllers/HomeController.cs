7dba66c5bbba34e0ac780b6f96d9274f467dda4a
Damian Salata <damiansalata10@gmail.com>
2020-11-25 14:10:43 +0100

    Add error page to identity server

--- src/Services/Identity/Identity.API/Controllers/HomeController.cs
+++ src/Services/Identity/Identity.API/Controllers/HomeController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Identity.API.ViewModels;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Identity.API.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public HomeController(IIdentityServerInteractionService interaction)
+        {
+            _interaction = interaction;
+        }
+
         public IActionResult Index()
         {
-            return new RedirectResult("~/Account/Login");
+            var vm = new ErrorViewModel
+            {
+                Error = new IdentityServer4.Models.ErrorMessage()
+                {
+                    Error = "Page not found!"
+                }
+            };
+
+            return View("Error", vm);
+        }
+
+        public async Task<IActionResult> Error(string errorId)
+        {
+            var vm = new ErrorViewModel();
+
+            var message = await _interaction.GetErrorContextAsync(errorId);
+            if (message != null)
+            {
+                vm.Error = message;
+            }
+
+            return View("Error", vm);
         }
     }
 }
